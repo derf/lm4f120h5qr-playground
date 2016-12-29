353438804bb30b700c804cc087d37bce56530085
Daniel Friesel <derf@finalrewind.org>
2016-12-29 22:06:13 +0100

    ADC fuckery

--- src/main.c
+++ src/main.c
@@ -1,5 +1,28 @@
+#define TARGET_IS_BLIZZARD_RA1
 #include <lm4f120h5qr.h>
+#include <rom.h>
+#include <rom_map.h>
+#include <hw_types.h>
+#include <hw_memmap.h>
+#include <hw_ints.h>
+#include <pin_map.h>
 #include <util.h>
+#include <gpio.h>
+#include <adc.h>
+#include <uart.h>
+
+#define UARTDR_0   *(int *)(0x4000c000)
+#define UARTRSR_0  *(int *)(0x4000c004)
+#define UARTECR_0  *(int *)(0x4000c004)
+#define UARTFR_0   *(int *)(0x4000c018)
+#define UARTILPR_0 *(int *)(0x4000c020)
+#define UARTIBRD_0 *(int *)(0x4000c024)
+#define UARTFBRD_0 *(int *)(0x4000c028)
+#define UARTLCRH_0 *(int *)(0x4000c02c)
+#define UARTCTL_0  *(int *)(0x4000c030)
+#define UARTIFLS_0 *(int *)(0x4000c034)
+#define UARTIM_0   *(int *)(0x4000c038)
+#define UARTCC_0   *(int *)(0x4000cfc8)
 
 void delay(unsigned long max)
 {
@@ -10,6 +33,53 @@ void delay(unsigned long max)
 
 	for (counter = 0; counter < max; counter++)
 		__asm("nop");
+}
+
+void uart_putchar(char c)
+{
+	ROM_UARTCharPutNonBlocking(UART0_BASE, c);
+}
+
+void uart_putdigit(unsigned char digit)
+{
+	if (digit < 10)
+		uart_putchar('0' + digit);
+	else
+		uart_putchar('A' + digit - 10);
+}
+
+void uart_putfloat(float num)
+{
+	if (num > 10000)
+		uart_putdigit(((int)num % 100000) / 10000);
+	if (num > 1000)
+		uart_putdigit(((int)num % 10000) / 1000);
+	if (num > 100)
+		uart_putdigit(((int)num % 1000) / 100);
+	if (num > 10)
+		uart_putdigit(((int)num % 100) / 10);
+	uart_putdigit((int)num % 10);
+	uart_putchar('.');
+	uart_putdigit((int)(num * 10) % 10);
+	uart_putdigit((int)(num * 100) % 10);
+}
+
+void UARTIntHandler(void)
+{
+	unsigned long ulStatus;
+
+	ulStatus = ROM_UARTIntStatus(UART0_BASE, true);
+	ROM_UARTIntClear(UART0_BASE, ulStatus);
+	while (ROM_UARTCharsAvail(UART0_BASE)) {
+		ROM_UARTCharPutNonBlocking(UART0_BASE, ROM_UARTCharGetNonBlocking(UART0_BASE));
+	}
+}
+
+void UARTSend(const unsigned char *buf, unsigned long len)
+{
+	while (len--) {
+		ROM_UARTCharPutNonBlocking(UART0_BASE, *buf++);
+	}
 }
 
 int main(void)
@@ -23,26 +93,51 @@ int main(void)
 		_BV(1) | _BV(3)
 	};
 	unsigned char i, j;
+	unsigned long adcval;
 
-	 /* Enable PORTF */
-	SYSCTL_RCGC2_R = SYSCTL_RCGC2_GPIOF;
+	ROM_SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN |
+		SYSCTL_XTAL_16MHZ);
 
-	__asm("nop");
+	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+	ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3 | GPIO_PIN_2 | GPIO_PIN_1);
+	ROM_IntMasterEnable();
+	ROM_GPIOPinConfigure(GPIO_PA0_U0RX);
+	ROM_GPIOPinConfigure(GPIO_PA1_U0TX);
+	ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 
-	/* Set PF1, PF2, PF3 as output */
-	GPIO_PORTF_DIR_R = 0x0e;
+	ROM_UARTConfigSetExpClk(UART0_BASE, ROM_SysCtlClockGet(), 115200,
+		(UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
+		UART_CONFIG_PAR_NONE));
 
-	/* Enable Digital IO on PF1, PF2, PF3 */
-	GPIO_PORTF_DEN_R = 0x0e;
+	ROM_IntEnable(INT_UART0);
+	ROM_UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);
+
+	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
+	ROM_ADCSequenceConfigure(ADC0_BASE, 0, ADC_TRIGGER_PROCESSOR, 0);
+	ROM_ADCSequenceStepConfigure(ADC0_BASE, 0, 0, ADC_CTL_TS | ADC_CTL_IE | ADC_CTL_END);
+	ROM_ADCSequenceEnable(ADC0_BASE, 0);
+	ROM_ADCIntClear(ADC0_BASE, 0);
 
 	while(1) {
 		for (i = 0; i < sizeof(rgb); i++) {
 			for (j = 0; j < 25; j++) {
 				GPIO_PORTF_DATA_R = rgb[i];
-				delay(500);
+				delay(400);
 				GPIO_PORTF_DATA_R = 0;
-				delay(5000);
+				delay(4000);
 			}
 		}
+		ROM_ADCProcessorTrigger(ADC0_BASE, 0);
+		while(!ROM_ADCIntStatus(ADC0_BASE, 0, false)) ;
+		ROM_ADCIntClear(ADC0_BASE, 0);
+		ROM_ADCSequenceDataGet(ADC0_BASE, 0, &adcval);
+		uart_putfloat(adcval);
+		uart_putchar(' ');
+		uart_putfloat(147 - ((75 * adcval * 33)) / 40960);
+		uart_putchar('C');
+		delay(1000);
+		uart_putchar(' ');
 	}
 }
